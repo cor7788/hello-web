d442231ee5c4f0657a042a0a3c11c9a87a84e45f
cor <cor90@qq.com>
2019-10-15 18:11:54 +0800

    Initial commit

---
+++ hello-servlet/src/main/java/com/example/web/servlet/FooServlet.java
@@ -0,0 +1,33 @@
+package com.example.web.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/foo")
+public class FooServlet implements Servlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("foo servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
